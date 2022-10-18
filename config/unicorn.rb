c56db68848e92418a47ad2d374a68df137d01927
TomooAkamatsu <t.akmt@ymail.ne.jp>
2022-10-18 20:29:14 +0900

    unicorn.rbの修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,29 +1,55 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
+# paths
+app_path = "/var/www/raisetech-live8-sample-app"
+pid_path = "#{app_path}/tmp/pids/unicorn.pid"
+listen_path = "#{app_path}/tmp/sockets/unicorn.sock"
+# unicorn paths
+working_directory app_path
+pid pid_path
+
+# listen
+listen listen_path, backlog: 64
+
+# logging
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+# workers
+worker_processes 2
+
+# Time-out
+timeout 30
+
+# use correct Gemfile on restarts
+before_exec do |_server|
+  ENV["BUNDLE_GEMFILE"] = "#{app_path}/current/Gemfile"
+end
+
+# preload
 preload_app true
 
-listen '/home/ec2-user/raisetech-live8-sample-app/unicorn.sock'
-pid    '/home/ec2-user/raisetech-live8-sample-app/unicorn.pid'
-
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
-  defined?(ActiveRecord::Base) and
+before_fork do |server, _worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  # rubocop: disable Lint/HandleExceptions
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+  # rubocop: enable Lint/HandleExceptions
 end
 
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-
+after_fork do |_server, _worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
